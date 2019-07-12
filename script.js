a35964ab50b9c82b1fe90884d012056943bc5adf
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-12 15:35:42 +0300

    - created a function, and added event listener to button1

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let btn1 = document.getElementById("gomb1");
+let btn2 = document.getElementById("gomb2");
+let szoveg1 = document.getElementById("szoveg1");
+let szoveg2 = document.getElementById("szoveg2");
+
+btn1.addEventListener("click", function () {
+    szoveg1.style.display = "none";
+});
