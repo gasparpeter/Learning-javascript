796e5bfa5371a7060c3ea2747dad8f7ba6820b56
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-13 23:08:03 +0300

    - close button actuated

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
+let dobozka = document.getElementById("dobozka");
 let btn1 = document.getElementById("gomb1");
 let btn2 = document.getElementById("gomb2");
 let szoveg1 = document.getElementById("szoveg1");
 let szoveg2 = document.getElementById("szoveg2");
+let closeBtn = document.getElementById("X");
 
 btn1.addEventListener("click", function () {
     szoveg1.style.display = "none";
@@ -10,4 +11,8 @@ btn1.addEventListener("click", function () {
 
 btn2.addEventListener("click", function () {
    szoveg2.innerText = "ASDADA";
+});
+
+closeBtn.addEventListener("click", function () {
+   dobozka.style.display = "none";
 });
