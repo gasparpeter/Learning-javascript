60e185b85733730d4c0d01889b4c9468dc993677
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-15 14:56:49 +0300

    - the generator is working

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ let btn = document.getElementById("btn");
 function generateItems() {
     let ul = document.createElement("ul");
     
-    for (let i = 0; i < 10; i++ ) {
+    for (let i = 0; i < 1; i++ ) {
         let li = document.createElement("li");
         ul.appendChild(li);
     }
+    document.body.appendChild(ul);
 }
 
 btn.addEventListener("click", generateItems);
